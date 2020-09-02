83f653288897914d9c8543b69d46e2db8bae6d57
andre <andre4130@gmail.com>
2020-09-02 09:02:48 +0200

    Updates

---
+++ precache-manifest.214bb0ceb70edd22d6d05c1ca8f8b53f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252ce57253131a2c108de902462f66ad",
+    "url": "/andre4130/GimmeVenue/index.html"
+  },
+  {
+    "revision": "6db6a684857a458f4902",
+    "url": "/andre4130/GimmeVenue/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dd120fe3f11e0fc5811e",
+    "url": "/andre4130/GimmeVenue/static/css/main.615a1c46.chunk.css"
+  },
+  {
+    "revision": "6db6a684857a458f4902",
+    "url": "/andre4130/GimmeVenue/static/js/2.6bb8df33.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/andre4130/GimmeVenue/static/js/2.6bb8df33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd120fe3f11e0fc5811e",
+    "url": "/andre4130/GimmeVenue/static/js/main.c5d70a2f.chunk.js"
+  },
+  {
+    "revision": "a146208888fb62024db9",
+    "url": "/andre4130/GimmeVenue/static/js/runtime-main.fec89ee2.js"
+  },
+  {
+    "revision": "c8b67ff8ba8057886d6f6deb2bdf9f02",
+    "url": "/andre4130/GimmeVenue/static/media/guitar.c8b67ff8.svg"
+  },
+  {
+    "revision": "f74aa8f8c32ea46a12129246b8dfc0fb",
+    "url": "/andre4130/GimmeVenue/static/media/synthesizer.f74aa8f8.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/andre4130/GimmeVenue/precache-manifest.214bb0ceb70edd22d6d05c1ca8f8b53f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andre4130/GimmeVenue/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
